d0f2b73a491fce7c3c98a8f2a86160116e1e12a1
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-06-26 22:45:10 +0700

    create header and banner

---
+++ resources/views/home.blade.php
@@ -0,0 +1,18 @@
+@extends('layouts.app')
+
+@section('content')
+    <section class="banner">
+        <div class="container-banner">
+            <div class="content">
+                <div class="content-item">learn anytime, anywhere</div>
+                <div class="content-title"><span>at HapoLearning</span>
+                    <img src="{{ asset('images/hapo.png') }}" alt="icon-hapo">!
+                </div>
+                <div class="content-description ">interactive lessons, "on-the-go" practice, peer support</div>
+                <div class="content-btn"><a href="#" class="btn-start">start learning now!</a></div>
+            </div>
+        </div>
+        <div class="effect-banner"></div>
+    </section>
+
+@endsection
---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,99 @@
+<footer class="text-center text-lg-start bg-light text-muted">
+    <section class="d-flex justify-content-center justify-content-lg-between p-4 border-bottom">
+        <div>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-twitter"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-google"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-instagram"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="" class="me-4 text-reset">
+                <i class="fab fa-github"></i>
+            </a>
+        </div>
+    </section>
+
+    <section class="">
+        <div class="container text-center text-md-start mt-5">
+            <div class="row mt-3">
+                <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+                    <h6 class="text-uppercase fw-bold mb-4">
+                        <i class="fas fa-gem me-3"></i>Hapo Learn
+                    </h6>
+                    <p>
+                        Interactive lessons, "on-the-go" practive, peerm support
+                    </p>
+                </div>
+
+                <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+{{--                    <h6 class="text-uppercase fw-bold mb-4">--}}
+{{--                        Products--}}
+{{--                    </h6>--}}
+                    <p>
+                        <a href="#!" class="text-reset">Home</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Features</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Courses</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Blog</a>
+                    </p>
+                </div>
+
+                <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+{{--                    <h6 class="text-uppercase fw-bold mb-4">--}}
+{{--                        Useful links--}}
+{{--                    </h6>--}}
+                    <p>
+                        <a href="#!" class="text-reset">Contact</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">Terms of Use</a>
+                    </p>
+                    <p>
+                        <a href="#!" class="text-reset">FAQ</a>
+                    </p>
+{{--                    <p>--}}
+{{--                        <a href="#!" class="text-reset">Help</a>--}}
+{{--                    </p>--}}
+                </div>
+
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+{{--                    <h6 class="text-uppercase fw-bold mb-4">--}}
+{{--                        Contact--}}
+{{--                    </h6>--}}
+{{--                    <p><i class="fas fa-home me-3"></i> New York, NY 10012, US</p>--}}
+{{--                    <p>--}}
+{{--                        <i class="fas fa-envelope me-3"></i>--}}
+{{--                        info@example.com--}}
+{{--                    </p>--}}
+{{--                    <p><i class="fas fa-phone me-3"></i> + 01 234 567 88</p>--}}
+{{--                    <p><i class="fas fa-print me-3"></i> + 01 234 567 89</p>--}}
+                    <div class="row">
+                        <div><i class="fa-brands fa-facebook"></i></div>
+                        <div><i class="fa-solid fa-phone"></i></div>
+                        <div><i class="fa-solid fa-envelope"></i></div>
+{{--                        chưa bắt fontaresomeone--}}
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <div class="text-center p-4" style="background-color: rgba(0, 0, 0, 0.05);">
+        © 2021 Copyright:
+        <a class="text-reset fw-bold" href="https://mdbootstrap.com/">MDBootstrap.com</a>
+    </div>
+</footer>

4e97d4a87b53e2cf62a83ffd6057aed1e5af51c4
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-06-28 17:26:13 +0700

    #7:create statistic-banner

---
+++ resources/views/layouts/statistic.blade.php
@@ -0,0 +1,14 @@
+<div class="container-fluid statistic-banner">
+   <div class="static-container">
+       <div class="banner-static-title">Become a member of our<br>
+           growing community!
+       </div>
+       <div class="static-btn">
+           Start Learning Now!
+       </div>
+   </div>
+</div>
+
+<div class="container">
+
+</div>

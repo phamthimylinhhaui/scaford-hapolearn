059f5220e7281b8e2c173f4b62b8661a7e3cf8a2
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-06-29 11:57:29 +0700

    #9:create footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,8 +1,8 @@
 <footer class="text-center text-lg-start bg-light text-muted">
     <section class="container-fluid footer-top">
-        <div class="container text-center text-md-start mt-5">
-            <div class="row mt-3">
-                <div class="col-md-3 col-lg-4 col-xl-4 mx-auto mb-4 footer-brand">
+        <div class="text-center text-md-start mt-5">
+            <div class="row mt-3 footer-item">
+                <div class="col-md-4 col-lg-4 col-xl-4 mx-auto mb-4 footer-brand">
                     <a class="footer-logo" href="#">
                         <img class="logo-image" src="{{ asset('images/logo-white.png') }}" alt="HapoLearn Logo">
                     </a>
@@ -11,7 +11,7 @@
                     </p>
                 </div>
 
-               <div class="row-item col-4">
+                <div class="col-md-4 col-lg-4 col-xl-4 col-xs-12 row-item">
                    <div class="col-6">
                        <p>
                            <a href="#!" class="text-reset">Home</a>
@@ -37,17 +37,14 @@
                        <p>
                            <a href="#!" class="text-reset">FAQ</a>
                        </p>
-                       {{--                    <p>--}}
-                       {{--                        <a href="#!" class="text-reset">Help</a>--}}
-                       {{--                    </p>--}}
                    </div>
                </div>
 
-                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
-                    <div class="row">
-                        <div><i class="fa-brands fa-facebook"></i></div>
-                        <div><i class="fa-solid fa-phone"></i></div>
-                        <div><i class="fa-solid fa-envelope"></i></div>
+                <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4 row-icon">
+                    <div class="row footer-icon">
+                        <div class="col-4 list-icon"><a class="text-reset" href="facebook.com/tuyen.dung.haposoft"><i class="fa-brands fa-facebook"></i></a></div>
+                        <div class="col-4 list-icon"><a class="text-reset" href="+84-85-645-9898"><i class="fa-solid fa-phone"></i></a></div>
+                        <div class="col-4 list-icon"><a class="text-reset" href="info@haposoft.com"><i class="fa-solid fa-envelope"></i></a></div>
                     </div>
                 </div>
             </div>

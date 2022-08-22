d939c8d1dfc722f6d052c2077258348767bea30f
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-08-22 21:41:28 +0700

    #39: FIX complete send email

--- app/Http/Controllers/Auth/ForgotPasswordController.php
+++ app/Http/Controllers/Auth/ForgotPasswordController.php
@@ -3,7 +3,11 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Foundation\Auth\SendsPasswordResetEmails;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Str;
 
 class ForgotPasswordController extends Controller
 {
@@ -19,4 +23,24 @@ class ForgotPasswordController extends Controller
     */
 
     use SendsPasswordResetEmails;
+
+    public function sendResetLinkEmail(Request $request)
+    {
+        $this->validateEmail($request);
+        $token = strtoupper(Str::random(10));
+        $user = User::where('email', $request->email)->first();
+
+        if (empty($user)) {
+            return redirect()->back()->with('error', __('message.email_dont_exists'));
+        }
+
+        $user->update(['remember_token', $token]);
+
+        Mail::send('auth.passwords.mail_send', compact('user'), function ($email) use ($user) {
+            $email->subject('HapoLearn - Lấy lại mật khẩu');
+            $email->to($user->email);
+        });
+
+        return redirect()->back()->with('success', __('message.success_send_mail'))->with(compact('user'));
+    }
 }

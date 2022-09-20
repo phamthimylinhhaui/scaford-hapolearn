8b1345476efa2dd6cb241a3719b6557863e757cd
phamthimylinhhaui <phamthimylinhhaui@gmail.com>
2022-09-21 06:13:34 +0700

    [#44]: test feature

--- tests/Feature/Users/GetListUserTest.php
+++ tests/Feature/Users/GetListUserTest.php
@@ -36,5 +36,9 @@ class GetListUserTest extends TestCase
     public function checkBo()
     {
         $this->assertTrue(true);
+        $expected = "Hoang";
+        $actual = "Hoang";// minh hoang thi test no fail
+        $this->assertEquals($expected, $actual, 'ktra kq mong đợi có bằng kết quả khi chạy k');
+        $this->assertEmpty($expected, 'check xem biến này có rỗng không');
     }
 }
